10a11cc7338a3955bf18ed0f7d1a00794fe3026f
Fabian Marcus <apps@fabian-marcus.de>
2019-09-29 22:42:49 +0200

    Fix für Anzeige "#Skills" Headline

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,78 +1,80 @@
 <template>
-    <section class="skills">
+    <section>
         <div class="box">
             <h2>#Skills</h2>
             <hr />
         </div>
-        <div class="skillset">
-            <article>
-                <div class="skillimg">
-                    <img src="/skills/javascript.png" v-in-viewport.once="{margin: '0%'}" />
-                </div>
-                <h3>Javascript</h3>
-                <p>
-                    Javascript durchlebt viele Veränderungen. Beim gegebenen Tempo ist es schwer, immer auf dem
-                    Laufenden zu bleiben. Da Javascript aber gleichzeitig meine
-                    <strong>Lieblingsdisziplin</strong> bildet, macht es
-                    trotzdem Spaß! Egal ob React, Vue.js, Mithril oder Node.js: Sie kamen mir alle schon mal unter
-                    die Tastatur...
-                </p>
-            </article>
-            <article>
-                <div class="skillimg">
-                    <img src="/skills/html_css.png" v-in-viewport.once="{margin: '0%'}" />
-                </div>
-                <h3>HTML+SCSS</h3>
-                <p>
-                    Zusammen mit Javascript bilden HTML und CSS den Dreiklang der Frontendentwicklung; SCSS rundet die Sache ab.
-                    Mit einem Auge für Abstände, Farben und einem gezielten Maß an Animationen macht es mir immer wieder Spaß,
-                    schöne, saubere & UX freundliche Designs zu entwickeln! Alles
-                    <strong>responsive & mobile first</strong> natürlich...
-                </p>
-            </article>
-            <article>
-                <div class="skillimg">
-                    <img src="/skills/gcloud.png" v-in-viewport.once="{margin: '0%'}" />
-                </div>
-                <h3>Cloud</h3>
-                <p>
-                    Warum selbst hosten, wenn es andere besser können - z.B. Google. Cloud Computing mit
-                    <strong>Firestore</strong> &
-                    <strong>Cloud/Serverless Functions</strong> bringt das Backend as a Service und verkürzt die Entwicklungszeit.
-                    Aber egal ob Google, AWS oder ein einfach Netlify: Es ist ein spannendes Thema!
-                </p>
-            </article>
-            <article>
-                <div class="skillimg">
-                    <img src="/skills/sql.png" v-in-viewport.once="{margin: '0%'}" />
-                </div>
-                <h3>SQL</h3>
-                <p>
-                    Geht es nicht ohne Backend, sind relationale Datenbanken oft nicht verkehrt, um Daten zu speichern.
-                    Auch hiermit arbeite ich immer wieder gerne - z.B. um eine API zu entwickeln.
-                </p>
-            </article>
-            <article>
-                <div class="skillimg">
-                    <img src="/skills/orm.jpg" v-in-viewport.once="{margin: '0%'}" />
-                </div>
-                <h3>ORM</h3>
-                <p>
-                    Queries immer selbst schreiben? Nicht immer notwendig. Ein objektrelationle Mapping erleichtert das
-                    Leben für Standardabfragen ungemein und verkürzt die Entwicklungszeit. In Node.js zum Beispiel Sequelize.
-                </p>
-            </article>
-            <article>
-                <div class="skillimg">
-                    <img src="/skills/restapi.png" v-in-viewport.once="{margin: '0%'}" />
-                </div>
-                <h3>REST</h3>
-                <p>
-                    Da Frontend und Backend heutzutage meist getrennt werden, findet die Datenübergabe bei mir meist
-                    im <strong>JSON-Format</strong> als Teil einer REST-API statt. Gerade im Javascript-Umfeld bietet sich dadurch eine
-                    einfache Schnittestelle, Daten auszutauschen.
-                </p>
-            </article>
+        <div class="skills">
+            <div class="skillset">
+                <article>
+                    <div class="skillimg">
+                        <img src="/skills/javascript.png" v-in-viewport.once="{margin: '0%'}" />
+                    </div>
+                    <h3>Javascript</h3>
+                    <p>
+                        Javascript durchlebt viele Veränderungen. Beim gegebenen Tempo ist es schwer, immer auf dem
+                        Laufenden zu bleiben. Da Javascript aber gleichzeitig meine
+                        <strong>Lieblingsdisziplin</strong> bildet, macht es
+                        trotzdem Spaß! Egal ob React, Vue.js, Mithril oder Node.js: Sie kamen mir alle schon mal unter
+                        die Tastatur...
+                    </p>
+                </article>
+                <article>
+                    <div class="skillimg">
+                        <img src="/skills/html_css.png" v-in-viewport.once="{margin: '0%'}" />
+                    </div>
+                    <h3>HTML+SCSS</h3>
+                    <p>
+                        Zusammen mit Javascript bilden HTML und CSS den Dreiklang der Frontendentwicklung; SCSS rundet die Sache ab.
+                        Mit einem Auge für Abstände, Farben und einem gezielten Maß an Animationen macht es mir immer wieder Spaß,
+                        schöne, saubere & UX freundliche Designs zu entwickeln! Alles
+                        <strong>responsive & mobile first</strong> natürlich...
+                    </p>
+                </article>
+                <article>
+                    <div class="skillimg">
+                        <img src="/skills/gcloud.png" v-in-viewport.once="{margin: '0%'}" />
+                    </div>
+                    <h3>Cloud</h3>
+                    <p>
+                        Warum selbst hosten, wenn es andere besser können - z.B. Google. Cloud Computing mit
+                        <strong>Firestore</strong> &
+                        <strong>Cloud/Serverless Functions</strong> bringt das Backend as a Service und verkürzt die Entwicklungszeit.
+                        Aber egal ob Google, AWS oder ein einfach Netlify: Es ist ein spannendes Thema!
+                    </p>
+                </article>
+                <article>
+                    <div class="skillimg">
+                        <img src="/skills/sql.png" v-in-viewport.once="{margin: '0%'}" />
+                    </div>
+                    <h3>SQL</h3>
+                    <p>
+                        Geht es nicht ohne Backend, sind relationale Datenbanken oft nicht verkehrt, um Daten zu speichern.
+                        Auch hiermit arbeite ich immer wieder gerne - z.B. um eine API zu entwickeln.
+                    </p>
+                </article>
+                <article>
+                    <div class="skillimg">
+                        <img src="/skills/orm.jpg" v-in-viewport.once="{margin: '0%'}" />
+                    </div>
+                    <h3>ORM</h3>
+                    <p>
+                        Queries immer selbst schreiben? Nicht immer notwendig. Ein objektrelationle Mapping erleichtert das
+                        Leben für Standardabfragen ungemein und verkürzt die Entwicklungszeit. In Node.js zum Beispiel Sequelize.
+                    </p>
+                </article>
+                <article>
+                    <div class="skillimg">
+                        <img src="/skills/restapi.png" v-in-viewport.once="{margin: '0%'}" />
+                    </div>
+                    <h3>REST</h3>
+                    <p>
+                        Da Frontend und Backend heutzutage meist getrennt werden, findet die Datenübergabe bei mir meist
+                        im <strong>JSON-Format</strong> als Teil einer REST-API statt. Gerade im Javascript-Umfeld bietet sich dadurch eine
+                        einfache Schnittestelle, Daten auszutauschen.
+                    </p>
+                </article>
+            </div>
         </div>
     </section>
 </template>
